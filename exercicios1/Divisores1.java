d72d28ff80c661fcbb8e0ac390104c38987e1cbc
AndersonHomma <andersonhomma@gmail.com>
2024-09-14 18:24:17 -0300

    Resolvendo exercicios

---
+++ exercicios1/Divisores1.java
@@ -0,0 +1,19 @@
+package exercicios1;
+
+public class Divisores1 {
+
+    /*
+        Ler um número inteiro N e calcular todos os seus divisores.
+        Ex entrada: 6
+
+        Entrada
+        O arquivo de entrada contém um valor inteiro.
+
+        Saída
+        Escreva todos os divisores positivos de N, um valor por linha.
+        Ex saída: 1/2/3/6
+     */
+    public static void main(String[] args) {
+
+    }
+}
--- exercicios1/Positivos_Media.java
+++ exercicios1/Positivos_Media.java
@@ -1,17 +1,49 @@
 package exercicios1;
 
+import java.util.Scanner;
+
 public class Positivos_Media {
     /*
         Leia 6 valores. Em seguida, mostre quantos destes valores
-         digitados foram positivos. Na próxima linha,
-         deve-se mostrar a média de todos os valores positivos
-         digitados, com um dígito após o ponto decimal.
+        digitados foram positivos. Na próxima linha,
+        deve-se mostrar a média de todos os valores positivos
+        digitados, com um dígito após o ponto decimal.
 
-         Ex entrada: 7/-5/6/-3.4/4.6/12
-         Ex saída: 4 valores positivos/7.4
+        Entrada
+        A entrada contém 6 números que podem ser valores inteiros
+        ou de ponto flutuante. Pelo menos um destes números
+        será positivo.
+
+        Saída
+        O primeiro valor de saída é a quantidade de valores
+        positivos. A próxima linha deve mostrar a média dos
+        valores positivos digitados.
+
+        Ex entrada: 7/-5/6/-3.4/4.6/12
+        Ex saída: 4 valores positivos/7.4
     */
 
     public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        double[] numeros = new double[6];
+        float n;
+        double soma = 0;
+        int qtdPos = 0;
+
+        for (int i=0; i <=5; i++){
+            System.out.printf("Digite %d° número : ", i+1);
+            n = scan.nextFloat();
+            numeros[i] = n;
+            if(numeros[i]>0){
+                soma+=numeros[i];
+                qtdPos++;
+            }
+
+        }
+
+        System.out.printf("Dos números digitados %d são posisitos.",qtdPos);
+        System.out.printf("\nA média dos valores positivos é = %1f.",soma/qtdPos);
 
     }
 }
